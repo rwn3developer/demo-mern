9b62d61ae4c9748bfb9864867712b22006c7c94b
milansir <rwn3developer11@gmail.com>
2024-03-30 17:20:23 +0530

    home page setup work running

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -14,11 +14,15 @@ const Header = () => {
                         </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                                
                                 <li className="nav-item">
                                     <Link to={`/`} className="nav-link active" aria-current="page">Home</Link>
                                 </li>
                                 <li className="nav-item">
                                     <Link to={`/product`} className="nav-link active" aria-current="page">Product</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to={`/cart`} className="nav-link active" aria-current="page">Cart</Link>
                                 </li>
                                 <li className="nav-item">
                                     <Link className="nav-link">Contact</Link>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,15 +5,32 @@ import Header from '../component/Header'
 import Slider from '../component/Slider'
 
 const Home = () => {
-  return (
-    <div>
-        <Header/>
-        <Slider />
-        <div className='best-content'>
-            
+    return (
+        <div>
+            <Header /><br></br><br></br>
+            <Slider />
+            <div className='best-content'>
+                <div className="container">
+                    <div className="row">
+                        <h4>Best Mobile</h4>
+                        <div className="col-lg-3">
+                            <div className="card p-3">
+                                <img className="card-img-top" src="https://rukminim2.flixcart.com/image/612/612/xif0q/keyboard/gaming-keyboard/j/8/w/f2023-aula-original-imagr4m2gjqgfgag.jpeg?q=70" alt="Card image cap" />
+                                <div className="card-body">
+                                    <p className="card-title">Card title</p>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div>
+
+
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Home
